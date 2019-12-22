520abdfa13ce50e024bc31d30a3a9294174c346b
nghip123 <nghiamap2014@gmail.com>
2019-12-22 16:09:10 +0700

    dâd0

---
+++ bookstore/bookstore/Index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bookstore
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //nguyen son test
+            // chu code4
+            // them dong nua
+        }
+    }
+}
